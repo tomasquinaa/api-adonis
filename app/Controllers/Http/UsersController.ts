2505b24e65a34f8263a893f0e2d73bb74308b4c9
Armando Quinanga <armando.quinanga@RCSPRTT01.rcsangola.local>
2023-06-26 13:57:56 +0100

    adicionado no ar

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,47 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import { DateTime } from 'luxon'
+
+export default class UsersController {
+  public async index({}: HttpContextContract) {
+    const user = await User.all()
+    return user 
+  }
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({ request }: HttpContextContract) {
+    const body = request.only(['name', 'email', 'password'])
+    const user = await User.create({
+      name: body.name,
+      email: body.email,
+      password: body.password,
+    })
+
+    console.log(user.$isPersisted)
+    return user
+  }
+
+  public async show({ request }: HttpContextContract) {
+    const userId = request.param('id')
+    const user = await User.findOrFail(userId)
+    return user
+  }
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({ request }: HttpContextContract) {
+    const userId = request.param('id')
+    const body = request.only(['name', 'email', 'password'])
+    const user = await User.findOrFail(userId)
+    await user?.merge(body).save()
+    return user
+  }
+
+  public async destroy({ request }: HttpContextContract) {
+    const userId = request.param('id')
+    const user = await User.findOrFail(userId)
+    await user.delete()
+    return true
+  }
+}
---
+++ start/routes.ts
@@ -0,0 +1,45 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/hello', async () => {})
+
+Route.post('login', async ({ auth, request, response }) => {
+  const email = request.input('email')
+  const password = request.input('password')
+
+  try {
+    const token = await auth.use('api').attempt(email, password)
+    return token
+  } catch {
+    return response.unauthorized('Invalid credentials')
+  }
+
+})
+
+Route.get('dashboard', async ({ auth }) => {
+  await auth.use('api').authenticate()
+  console.log()
+  return `Olá ${auth.user.name}, voce esta autenticado(a)!`
+})
+
+//Route.get('/users', 'UsersController.index')
+Route.resource('users', 'UsersController')
